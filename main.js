96945a7464a0bf48b542646a97b9344d422300cc
Chris Tralie <chris.tralie@gmail.com>
2021-11-20 18:54:01 -0500

    Making some changes

--- main.js
+++ main.js
@@ -3,43 +3,40 @@
 // https://www.tensorflow.org/js/guide/train_models
 
 
-const shape = [2, 2];
-
-const xs = tf.randomNormal([100, 2]);
+const xs = tf.randomNormal([1000, 2]);
 
 xs.array().then(x => {
   let ys = [];
   for (let i = 0; i < x.length; i++) {
     let d = Math.sqrt(x[i][0]*x[i][0] + x[i][1]*x[i][1]);
-    if (d < 1) {
-      ys.push(1);
+    if (d < 0.5) {
+      ys.push(0); // Label as 0 if it's within a radius of 0.5
     }
     else {
-      ys.push(0);
+      ys.push(1);
     }
   }
+  let ysorig = ys;
   ys = tf.tensor(ys);
 
   // Create an arbitrary graph of layers, by connecting them
   // via the apply() method.
   const input = tf.input({shape: [2]});
-  const dense1 = tf.layers.dense({units: 3, activation: 'relu'}).apply(input);
-  const dense2 = tf.layers.dense({units: 1, activation: 'relu'}).apply(dense1);
+  const dense1 = tf.layers.dense({units: 3, activation: 'sigmoid', useBias:true}).apply(input);
+  const dense2 = tf.layers.dense({units: 1, activation: 'sigmoid', useBias:true}).apply(dense1);
   const model = tf.model({inputs: input, outputs: dense2});
+  //tfvis.show.modelSummary({name: 'Model Summary'}, model);
 
-  model.compile({loss: 'meanSquaredError',
-                optimizer: 'sgd'});
-
-  //let ys = [0, 1];
-
-
-  // Train the model using the data.
+  model.compile({loss: tf.losses.sigmoidCrossEntropy,
+                optimizer: tf.train.adam()});
+  
+  // Train the model using the data, and test on the same data for now
   model.fit(xs, ys, {epochs: 10}).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    const test = tf.tensor([0, 0]);
-    test.reshape([2]);
-    model.predict(test).print();
-    // Open the browser devtools to see the output
+    model.predict(xs).array().then(d => {
+      for (let i = 0; i < d.length; i++) {
+        console.log(d[i] + ", " + ysorig[i]);
+      }
+    })
   });
 
 
